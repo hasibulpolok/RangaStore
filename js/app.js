282b0027858d000d7b126946647f964ae4aaee55
Hasibul Polok <47984658+hasibulpolok@users.noreply.github.com>
2021-09-13 14:21:49 +0600

    changes text

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p>Total Sell : ${product.rating.count}</p>
+      <p>Total Reviews : ${product.rating.count}</p>
       <p>Rating : ${product.rating.rate} </p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
