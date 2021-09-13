0ea5c280eb0a833b2a124543aeedb8c62cdf0a93
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-09-13 06:22:23 +0600

    project complete

--- js/app.js
+++ js/app.js
@@ -1,55 +1,73 @@
+
 const loadProducts = () => {
-  // const url = `https://fakestoreapi.com/products`;
-  const url = `http://127.0.0.1:5500/db.json`;
+  const searchField = document.getElementById('input-field');
+  const searchText = searchField.value;
+  if (searchField.value == "") {
+    const invalidSearch = document.getElementById("emptytext");
+    invalidSearch.innerText = `Invalid Search! Please Write Something`;
+    return;
+  }
+
+  searchField.value = "";
+
+
+  const url = `https://fakestoreapi.com/products`;
+  // const url = `http://127.0.0.1:5500/db.json`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
+
 };
 
-
-// show all product in UI 
+// show all product in UI
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.image;
     const div = document.createElement("div");
-    div.classList.add("product")
+    div.classList.add("product");
     div.innerHTML = `<div class="single-product">
+      <div>
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p>Average Rating: ${product.rating.rate}</p>
+      <p>Total Sell : ${product.rating.count}</p>
+      <p>Total Rating Number : ${product.rating.rate} </p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button></div>
-
+      <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Details
+</button>
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel"></h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <h3>${product.description}</h3>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
 </div>
+
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
+  updateTotal();
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
@@ -64,8 +82,8 @@ const getInputValue = (id) => {
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
-  let total = parseFloat(convertedOldPrice) + convertPrice;
-  document.getElementById(id).innerText = (total.toFixed(2));
+  const total = convertedOldPrice + convertPrice;
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -90,16 +108,12 @@ const updateTaxAndCharge = () => {
   }
 };
 
-// const cartPrice = document.getElementById('price');
-// const cartPrice = document.getElementById('price');
-// const cartPrice = document.getElementById('price');
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
+    getInputValue("price") +
+    getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
-  console.log(grandTotal);
-  
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
-updateTotal();
+
